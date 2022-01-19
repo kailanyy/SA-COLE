35f9ea313d3ffe7088ab91ad7ab6c7ecbd956990
kailanyy <kailanyiaramenco@gmail.com>
2022-01-19 19:42:22 -0300

    Impressão dinamica de itens da lista na tela de registro de estoque de lixo

--- telas/form/registerCollectionPoint/script.js
+++ telas/form/registerCollectionPoint/script.js
@@ -1,17 +1,21 @@
 function validateFields() {
-    let cep = document.getElementById('cepField').value;
-    let rua = document.getElementById('ruaField').value;
-    let bairro = document.getElementById('bairroField').value;
-    let complemento = document.getElementById('complementoField').value;
+    let cep = document.getElementById('cep').value;
+    let localidade = document.getElementById('localidade').value;
+    let logradouro = document.getElementById('logradouro').value;
+    let bairro = document.getElementById('bairro').value;
+    let numero = document.getElementById('numero').value;
+    let complemento = document.getElementById('complemento').value;
 
-    if (!cep || !rua || !bairro || !complemento) {
+    if (!cep || !logradouro || !numero || !complemento || !bairro || !localidade) {
         alert('Todos os campos devem estar preenchidos!')
         return;
     }
     saveCollectionPoint({
         cep,
-        rua,
+        localidade,
+        logradouro,
         bairro,
+        numero,
         complemento
     })
 }
@@ -19,4 +23,46 @@ function validateFields() {
 function saveCollectionPoint(formFields) {
     console.log("Estamos dentro da função saveCollectionPoint, parametros: ");
     console.log(formFields);
+
+    let collectionPoint = localStorage.getItem("saveCollectionPoint");
+
+    if (collectionPoint == null) collectionPoint = [];
+    else collectionPoint = JSON.parse(collectionPoint);
+    collectionPoint.push(formFields);
+    localStorage.setItem("saveCollectionPoint", JSON.stringify(collectionPoint)) 
+    alert("Ponto de coleta cadastrado com sucesso!");
 }
+
+function listCollectionPoint() {
+    let collectionPoint = localStorage.getItem("saveCollectionPoint");
+    if (collectionPoint == null)
+    alert("Ainda não há pontos de coleta cadastrados.");
+    else {
+      let elementoTela = document.getElementById("listCollectionPoint")
+      collectionPoint = JSON.parse(collectionPoint);
+      elementoTela.innerHTML += '<ul class="list-group"> <br>';
+      collectionPoint.forEach(collectionPointInfo => {
+        elementoTela.innerHTML += '<li class="list-group-item" >CEP: ' + collectionPointInfo.cep + '</li>';
+        elementoTela.innerHTML += '<li class="list-group-item ">Cidade: ' + collectionPointInfo.localidade + '</li> <br>';
+        
+      });
+    }
+  }
+
+
+
+$("#cep").focusout(function(){
+    $.ajax({
+        url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
+        dataType: 'json',
+        
+        success: function(resposta){
+            $("#logradouro").val(resposta.logradouro);
+            $("#complemento").val(resposta.complemento);
+            $("#bairro").val(resposta.bairro);
+            $("#cidade").val(resposta.localidade);
+            $("#localidade").val(resposta.localidade);
+            $("#numero").focus();
+        }
+    });
+});
---
+++ telas/form/registerStockPoint/script.js
@@ -0,0 +1,97 @@
+let newPointItems = []
+
+$("#cep").focusout(function(){
+    $.ajax({
+        url: 'https://viacep.com.br/ws/'+$(this).val()+'/json/unicode/',
+        dataType: 'json',
+        
+        success: function(resposta){
+            $("#logradouro").val(resposta.logradouro);
+            $("#complemento").val(resposta.complemento);
+            $("#bairro").val(resposta.bairro);
+            $("#cidade").val(resposta.localidade);
+            $("#localidade").val(resposta.localidade);
+            $("#numero").focus();
+        }
+    });
+});
+
+function validateFields() {
+    let cep = document.getElementById('cep').value;
+    let localidade = document.getElementById('localidade').value;
+    let logradouro = document.getElementById('logradouro').value;
+    let bairro = document.getElementById('bairro').value;
+    let numero = document.getElementById('numero').value;
+    let complemento = document.getElementById('complemento').value;
+
+    if (!cep || !logradouro || !numero || !complemento || !bairro || !localidade) {
+        alert('Todos os campos devem estar preenchidos')
+        return;
+    }
+    saveStockPoint({
+        cep,
+        localidade,
+        logradouro,
+        bairro,
+        numero,
+        complemento
+    })
+}
+
+function saveStockPoint(formFields) {
+    console.log("Estamos dentro da função saveStockPoint, parametros: ");
+    console.log(formFields);
+
+    let stockPoint = localStorage.getItem("stockPoints");
+
+    if (stockPoint == null) stockPoint = [];
+    else stockPoint = JSON.parse(stockPoint);
+    stockPoint.push(formFields);
+    localStorage.setItem("stockPoints", JSON.stringify(stockPoint)) 
+    alert("Ponto de estoque cadastrado com sucesso!");
+}
+
+
+function validateNewListItem() {
+    let trashType = document.getElementById('trashType').value;
+    let amountTrash = document.getElementById('amountTrash').value;
+console.log(trashType, amountTrash);
+    if (!trashType || !amountTrash) {
+        alert('Todos os campos devem estar preenchidos')
+        return;
+    } 
+    addItem({
+        trashType,
+        amountTrash
+    })
+}
+
+function addItem(item) {
+    console.log(item);
+    newPointItems.push(item)
+    console.log(newPointItems);
+    printListItems();
+}
+
+function printListItems() {
+    let htmlListString = "";
+    for(i = 0; i < newPointItems.length; i++) {
+        htmlListString += 
+            "<tr>" +
+                `<th scope='row'>${i}</th>` +
+                `<td>${newPointItems[i].trashType}</td>` +
+                `<td>${newPointItems[i].amountTrash}</td>` +
+                `<td><button onclick="removeItemByIndex(${i})">Excluir</button></td>` +
+            "</tr>"
+    }
+
+    console.log(htmlListString);
+
+    document.getElementById('listRegisteredItems').innerHTML = htmlListString
+}
+
+function removeItemByIndex(index) {
+    console.log(index);
+    newPointItems.splice(index, 1)
+    printListItems();
+}
