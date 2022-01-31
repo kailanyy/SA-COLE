071f42a2d0e7743ffb0c33e94e4bd14e0809101c
Aluno <kailanyy>
2022-01-31 17:40:31 -0300

    iniciando a visualizacao dos pontos de coleta usuario

--- telas/form/userCollectionPoint/script.js
+++ telas/form/userCollectionPoint/script.js
@@ -1,8 +1,31 @@
-let map;
+// let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
+
+function listCollectionPoints() {
+  let collectionPoint = JSON.parse(localStorage.getItem("saveCollectionPoint"))
+  let loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+
+  
+  let userCollectionPoints = collectionPoint.filter(function(collectionPoint) {
+    return collectionPoint.userID === loggedUser.id 
   });
+
+  printListCollectionPoints(userCollectionPoints)
+  console.log("Filter:", userCollectionPoints);
+  console.log("listCollectionPoints:", listCollectionPoints);
 }
+
+
+function printListCollectionPoints(collectionPoint) {
+  let htmlListString = ""
+  console.log("collectionPoint.length", collectionPoint.length);
+  
+}
+
+listCollectionPoints()
