b334aa6324a93c7dfd51df7b253c493017f99f08
yasmin <yasmintitow@gmail.com>
2022-02-22 22:54:51 -0300

    area admin

--- screens/administrativeArea/user/script.js
+++ screens/administrativeArea/user/script.js
@@ -1,4 +1,38 @@
+function listarCadastros() {
+  let usuariosCadastrados = JSON.parse(localStorage.getItem("dadosCadastros"))
+  let htmlListString = `<table>
+              <thead>
+                <tr class="row100 head">
+                  <th class="cell100 column1">#</th>
+                  <th class="cell100 column2">Usuário</th>
+                  <th class="cell100 column6">Senha</th>
+                  <th class="cell100 column6">Ação</th>
+                </tr>
+              </thead>
+              <tbody id="listarCadastro">
+              </tbody>
+              </table>`
 
+  if (usuariosCadastrados == null){
+  alert("Ainda não há cadastros realizados.");
+
+  } else {
+     for(i = 0; i < usuariosCadastrados.length; i++) {
+
+        htmlListString += `<tbody>
+        <tr class="row100 body">
+            <td class="cell100 column1">${i}</td>
+            <td class="cell100 column2">${usuariosCadastrados[i].username}</td>
+            <td class="cell100 column5">${usuariosCadastrados[i].password}</td>
+            <td class="cell100 column1"><button class='btn btn-danger' onclick="removeItemByIndex(${i})">Excluir</button></td>
+        </tr>
+    </tbody>`
+
+     }
+  }
+
+  document.getElementById('listarCadastro').innerHTML = htmlListString
+}
 
 
 /* FUNÇÃO Q FUNCIONA
