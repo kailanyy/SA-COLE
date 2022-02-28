da4e3fba4e26f7ff3c5b69b41f4cbb6786863470
yasmin <yasmintitow@gmail.com>
2022-02-28 03:18:00 -0300

    estoque/pontos aparecendo direitinho agr, logo feita no login/cadastro, alterações no myAccount

--- screens/companyArea/myCollectionPoints/script.js
+++ screens/companyArea/myCollectionPoints/script.js
@@ -10,10 +10,11 @@ function listCollectionPoints() {
 }
 
 function printListCollectionPoints(collectionPoint) {
-  let htmlListString = ""
+  let htmlListString = "";
   
   for(i = 0; i < collectionPoint.length; i++) {
     htmlListString += 
+        `<ul class="list-group" style="width: 30%; margin: auto; margin-bottom: 2em; margin-top: 3em; box-shadow: 0px 10px 34px -15px rgb(0 0 0 / 24%)">` +
         `<li class="list-group-item" style="text-align: center; background-color: #3C8B63; color: white; padding: 13px;">Ponto de Coleta</li>` +
         `<li class="list-group-item bg-light"><b>CEP: </b>${collectionPoint[i].cep}</li>` +
         `<li class="list-group-item bg-light"><b>Cidade: </b>${collectionPoint[i].localidade}</li>` +
@@ -21,20 +22,40 @@ function printListCollectionPoints(collectionPoint) {
         `<li class="list-group-item bg-light"><b>Rua: </b>${collectionPoint[i].logradouro}</li>` +
         `<li class="list-group-item bg-light"><b>Número: </b>${collectionPoint[i].numero}</li>` +
         `<li class="list-group-item bg-light"><b>Complemento: </b>${collectionPoint[i].complemento}</li>` +
-        `<li class="list-group-item bg-light text-center"><b>Tipos de lixos aceitos</b>${getAcceptedTrash(collectionPoint[i].acceptedTrash)}</li>` +
-        `<li class="list-group-item"><button class='btn btn-danger w-100' onclick="removeItemById(${i})">Excluir</button></li>`
-}
+        `<li class="list-group-item bg-light text-center">${getAcceptedTrash(collectionPoint[i].acceptedTrash)}</li>` +
+        `<li class="list-group-item"><button class='btn btn-danger w-100' onclick="removeItemById(${i})">Excluir</button></li></ul>`
+    }
 
-document.getElementById('collectionPointsList').innerHTML = htmlListString
+      document.getElementById('collectionPointsList').innerHTML = htmlListString
 }
 
 function getAcceptedTrash(acceptedTrash) {
-  let htmlString = ""
-  for(i = 0; i < acceptedTrash.length; i++) {
-    htmlString +=
-    `<li class="list-group-item bg-light text-center">${acceptedTrash[i]}</li>` 
-  }
-  return htmlString;
+  let htmlString = `<div class="limiter">
+        <div class="wrap-table100">
+            <div class="table100 ver5" style="background-color: #F8F9FA;">
+                <div class="table100-head">
+                    <table>
+                        <thead>
+                            <tr class="row100 head">
+                                <th class="cell100 column9">Tipos de lixos aceitos</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+
+                <div class="table100-body js-pscroll">
+                    <table>
+                        <tbody>`
+
+    for(y = 0; y < acceptedTrash.length; y++) {
+      htmlString +=
+      `<tr class="row100 body">
+          <td class="cell100 column6">${acceptedTrash[y]}</td>
+      </tr>`
+
+    }
+
+    return htmlString + `</tbody></table></div></div></div></div>`;
 }
 
 function removeItemById(id) {
--- screens/userArea/myStockPoints/script.js
+++ screens/userArea/myStockPoints/script.js
@@ -17,6 +17,7 @@ function printListStockPoints(stockPoints) {
     for(i = 0; i < stockPoints.length; i++) {
         console.log(i);
         htmlListString += 
+            `<ul class="list-group" style="width: 30%; margin: auto; margin-bottom: 2em; margin-top: 3em; box-shadow: 0px 10px 34px -15px rgb(0 0 0 / 24%)">` +
             `<li class="list-group-item" style="text-align: center; background-color: #3c8b63; color: white; padding: 13px;">Ponto de Estoque</li>` +
             `<li class="list-group-item bg-light"><b>CEP:</b> ${stockPoints[i].cep}</li>` +
             `<li class="list-group-item bg-light"><b>Cidade:</b> ${stockPoints[i].localidade}</li>` +
@@ -25,10 +26,8 @@ function printListStockPoints(stockPoints) {
             `<li class="list-group-item bg-light"><b>Número:</b> ${stockPoints[i].numero}</li>` +
             `<li class="list-group-item bg-light"><b>Complemento:</b> ${stockPoints[i].complemento}</li>` +
             `${getHtmlStockPointItemsList(stockPoints[i].stockItems)}` +
-            `<li class="list-group-item mb-3"><button class='btn btn-danger w-100' onclick="removeItemById(${i})">Excluir</button></li>`
+            `<li class="list-group-item"><button class='btn btn-danger w-100' onclick="removeItemById(${i})">Excluir</button></li></ul>`
     }
-
-    console.log(htmlListString);
 
     document.getElementById('stockPointsList').innerHTML = htmlListString
 }
@@ -36,27 +35,34 @@ function printListStockPoints(stockPoints) {
 function getHtmlStockPointItemsList(stockItems) {
     
     console.log("Itens de estoque", stockItems);
-    let htmlString = "<table class='table table-light' style='text-align: center;'>" +
-        "<thead>" +
-          "<tr>" +
-            "<th scope='col'>Tipo de Lixo</th>" + 
-            "<th scope='col'>Quantidade</th>" + 
-          "</tr>" +
-        "</thead>" +
-        `<tbody>`
+    let htmlString = `<div class="limiter">
+                <div class="wrap-table100">
+                    <div class="table100 ver5" style="background-color: #F8F9FA;">
+                        <div class="table100-head">
+                            <table>
+                                <thead>
+                                    <tr class="row100 head">
+                                        <th class="cell100 column7">Tipo de lixo</th>
+                                        <th class="cell100 column8">Quantidade</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+
+                        <div class="table100-body js-pscroll">
+                            <table>
+                                <tbody>`
 
     for(y = 0; y < stockItems.length; y++) {
         console.log("for item");
         htmlString += 
-            "<tr class='table-light'>" +
-            `<td>${stockItems[y].trashType}</td>` +
-            `<td>${stockItems[y].amountTrash}</td>` +
-            "</tr>"
+        `<tr class="row100 body">
+            <td class="cell100 column6">${stockItems[y].trashType}</td>
+            <td class="cell100 column8">${stockItems[y].amountTrash}</td>
+        </tr>`
     }
 
-    console.log("htmlString:", htmlString);
-
-    return htmlString + "</tbody></table>";
+    return htmlString + `</tbody></table></div></div></div></div>`;
 
 } 
 
@@ -71,5 +77,28 @@ function removeItemById(id) {
     listStockPoints()
 }
 
-listStockPoints()
+(function ($) {
+	"use strict";
+	$('.column100').on('mouseover',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
 
+		$(table2).find("."+column).addClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).addClass('hov-column-head-'+ verTable);
+	});
+
+	$('.column100').on('mouseout',function(){
+		var table1 = $(this).parent().parent().parent();
+		var table2 = $(this).parent().parent();
+		var verTable = $(table1).data('vertable')+"";
+		var column = $(this).data('column') + ""; 
+
+		$(table2).find("."+column).removeClass('hov-column-'+ verTable);
+		$(table1).find(".row100.head ."+column).removeClass('hov-column-head-'+ verTable);
+	});
+    
+})(jQuery);
+
+listStockPoints()
