59b4d87870fcf954727da86f33d99a35ce2921e9
Kauanpradosenai <kauan_s_prado@estudante.sesisenai.org.br>
2022-03-08 17:47:42 -0300

    js minha conta

--- screens/account/user-profile.js
+++ screens/account/user-profile.js
@@ -7,19 +7,53 @@ function printLoggedUser(){
 
 }
 
-function removeItemById() {
-  let loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
-  let index = loggedUser.find(function(loggedUser){
-    return loggedUser.id === id
-  });
+function exitAccount() {
 
-  loggedUser.splice(index,1)
-  
-  localStorage.setItem("loggedUser", JSON.stringify(loggedUser)) 
-  
-  printLoggedUser()
+  Swal.fire({
+    title: 'Sair da conta?',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    cancelButtonText: 'Não!',
+    confirmButtonText: 'Sim!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+    window.location = "../../index.html"
+    }
+  })
 }
 
+function deleteAccount(id) {
+  let registeredUser = JSON.parse(localStorage.getItem("dadosCadastros"))
+
+  Swal.fire({
+    title: 'Você tem certeza?',
+    text: "Você não será capaz de reverter essa ação!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    cancelButtonText: 'Cancelar',
+    confirmButtonText: 'Sim, excluir'
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+    let index = registeredUser.find(function(registeredUser){
+        return registeredUser.id === id 
+    });
+    registeredUser.splice(index, 1)
+    localStorage.setItem("dadosCadastros", JSON.stringify(registeredUser))
+
+    // Swal.fire(
+    //   'Sua conta foi excluída com sucesso!',
+    //   'success'
+    // )
+
+    window.location = "../../index.html"
+    }
+  })
+}
 
 printLoggedUser()
-removeItemById()
