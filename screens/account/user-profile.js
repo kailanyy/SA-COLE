a534ee061778052f7cc753afb7a168f1e5c66e9f
Yasmin <yasmintitow@gmail.com>
2022-03-10 17:39:02 -0300

    crud minha conta

--- screens/account/user-profile.js
+++ screens/account/user-profile.js
@@ -1,3 +1,5 @@
+var iPassword = 0 
+
 function printLoggedUser(){
   let loggedUser = JSON.parse(localStorage.getItem("loggedUser")) 
 
@@ -13,16 +15,46 @@ function exitAccount() {
     title: 'Sair da conta?',
     icon: 'warning',
     showCancelButton: true,
-    confirmButtonColor: '#d33',
-    cancelButtonColor: '#3085d6',
-    cancelButtonText: 'Não!',
-    confirmButtonText: 'Sim!'
+    cancelButtonText: 'Voltar',
+    confirmButtonText: 'Sim'
   }).then((result) => {
     if (result.isConfirmed) {
 
     window.location = "../../index.html"
     }
   })
+}
+
+function changePassword(){
+  
+  (async () => {
+    const { value: password_id } = await Swal.fire({
+      title: 'Digite sua nova senha',
+      input: 'password',
+      icon: 'warning',
+      confirmButtonText: 'Confirmar',
+      cancelButtonText: 'Cancelar',
+      showCancelButton: true,
+      inputAttributes: {
+        maxlength: 10,
+        autocapitalize: 'off',
+        autocorrect: 'off'
+      }
+    })
+    
+    if (password_id) {
+      let registeredUser = JSON.parse(localStorage.getItem("dadosCadastros"))
+
+      for(i=0; i < registeredUser.length; i++){
+            iPassword = i
+          }
+  
+      registeredUser[iPassword].password = password_id
+      localStorage.setItem("dadosCadastros", JSON.stringify(registeredUser))
+      Swal.fire('Sua senha foi alterada com sucesso!', '', 'success')
+
+  }
+  })()
 }
 
 function deleteAccount(id) {
@@ -34,7 +66,6 @@ function deleteAccount(id) {
     icon: 'warning',
     showCancelButton: true,
     confirmButtonColor: '#d33',
-    cancelButtonColor: '#3085d6',
     cancelButtonText: 'Cancelar',
     confirmButtonText: 'Sim, excluir'
   }).then((result) => {
